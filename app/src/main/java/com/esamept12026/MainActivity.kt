d1f498489b92e808c45abc4b01ad58b616a288de
Rocchegiani Fabrizio <fabriziorocchegiani02@gmail.com>
2026-04-21 01:07:43 +0200

    Test

--- app/src/main/java/com/esamept12026/MainActivity.kt
+++ app/src/main/java/com/esamept12026/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.esamept12026.ui.theme.EsamePT12026Theme
 
 class MainActivity : ComponentActivity() {
+    //TEST
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
