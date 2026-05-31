52b6b07bc1eda4f2685791c5013b27c0a9ef9f48
Rocchegiani Fabrizio <fabriziorocchegiani02@gmail.com>
2026-05-31 03:53:37 +0200

    Aggiornamento dello stato utilizzato nell'applicazione e in particolar modo nella "Schermata 1" ("GameScreen").

--- app/src/main/java/com/esamept12026/data/GameState.kt
+++ app/src/main/java/com/esamept12026/data/GameState.kt
@@ -8,20 +8,18 @@ data class GameState(
     val sequence: List<String> = emptyList(),
     val userInput: List<String> = emptyList(),
     val userIndex: Int = 0,
-    //val clears: Int = 0,
 
     val gameStarted: Boolean = false,
+    val gamePaused: Boolean = false,
+    val hasStartedMatch: Boolean = false,
     val gameOver: Boolean = false,
 
     val locked: Boolean = false,
     val showing: Boolean = false,
     val error: Boolean = false,
 
-    val showExitDialog: Boolean = false,
+    //val showExitDialog: Boolean = false,
 
-    //val countdown: Int = 4,
-    //val isCountingDown: Boolean = false,
-    //val countdownActive: Boolean = true,
     val hasShownSequence: Boolean = false,
 
     val navigating: Boolean = false,
@@ -30,6 +28,5 @@ data class GameState(
     val highlighted: String? = null
 ){
     fun isInputEnabled(): Boolean =
-        //!locked && !showing && !countdownActive && !navigating
         !locked && !showing && !navigating
 }
