a5561075185b4ee14c0b298dbd844b9ba057d5d6
Rocchegiani Fabrizio <fabriziorocchegiani02@gmail.com>
2026-05-31 03:52:48 +0200

    Aggiunto indice di errore al risultato della partita.

--- app/src/main/java/com/esamept12026/model/GameResult.kt
+++ app/src/main/java/com/esamept12026/model/GameResult.kt
@@ -4,6 +4,6 @@ package com.esamept12026.model
     Classe dati che rappresenta la sequenza e i reset premuti dall'utente durante la sessione corrente.
 */
 data class GameResult(
-    val sequence: List<String>
-    //val clears: Int
+    val sequence: List<String>,
+    val errorIndex: Int
 )
